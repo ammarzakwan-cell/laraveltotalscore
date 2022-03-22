da33141d1b98dd6e2f6f90b1cd63acbe6494adbe
ammarzakwan-cell <ammaramran01@gmail.com>
2022-03-22 10:23:32 +0800

    initial release

--- resources/views/scores/index.blade.php
+++ resources/views/scores/index.blade.php
@@ -51,29 +51,7 @@
             </tr>
             @endforeach
         </table>
-        <p>
-        <?php
-            //servername
-            $servername = "localhost";
-            //username
-            $username = "root";
-            //empty password
-            $password = "";
-            //geek is the database name
-            $dbname = "laravel";
- 
-            // Create connection by passing these connection parameters
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            //sql query
-            $sql = "SELECT  SUM(total) from scores";
-            $result = $conn->query($sql);
-            //display data on web page
-            while($row = mysqli_fetch_array($result)){
-                echo " Total all: ". $row['SUM(total)'];
-                echo "<br>";
-            }
-        ?>
-        </p>
+        <?php include 'C:\Users\User\OneDrive\Desktop\Dev\Laravel\mysqlcalc\app\Service\total.php';?>
     {!! $scores->links() !!}
 </body>
 </html>
